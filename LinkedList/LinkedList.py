34600aa44e8d1981262263647d98ad7d3870cfc4
chipdelmal <chipdelmal@gmail.com>
2019-01-07 11:35:34 -0800

    Adding the basic traverse, and print traverse methods for the linked list

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -2,9 +2,33 @@
 # Linked List Class
 ##############################################################################
 
-import Node as n
+import Node as node
 
 
 class LinkedList:
     def __init__(self, head=None):
         self.head = head
+
+    def getHead(self):
+        return self.head
+
+    def traverseList(self):
+        tempNode = self.head
+        while tempNode.getNext() is not None:
+            tempNode = tempNode.getNext()
+        return tempNode
+
+    def printTraverseList(self):
+        tempNode = self.head
+        print(tempNode.getData())
+        while tempNode.getNext() is not None:
+            tempNode = tempNode.getNext()
+            print(tempNode.getData())
+
+    def addNode(self, newNode):
+        tail = self.traverseList()
+        tail.setNext(newNode)
+
+    def addValue(self, value):
+        tempNode = node.Node(value, None)
+        self.addNode(tempNode)
--- LinkedList/main.py
+++ LinkedList/main.py
@@ -1,7 +1,10 @@
 import LinkedList as ll
 import Node as n
 
-head = n.Node(10,None)
-print(head.getData())
-
-temp = ll.LinkedList(None)
+head = n.Node(10, None)
+llist = ll.LinkedList(head)
+llist.addValue(20)
+llist.printTraverseList()
+llist.addValue(30)
+llist.addValue(20)
+llist.printTraverseList()
