c2fed032d3ab297792baced3cee5f316c9682a83
chipdelmal <chipdelmal@gmail.com>
2019-01-07 13:05:29 -0800

    Changing folders structures to match the "Cracking the Coding Interview" for easy comparison

---
+++ 02_LinkedList/01_RemoveDuplicates.py
@@ -0,0 +1,8 @@
+import LinkedList as LinkedList
+
+
+class LinkedList_Remove(LinkedList):
+    def removeDuplicatesHash():
+        return 1
+    def removeDuplicatesNoBuffer():
+        return 1
--- LinkedList/01_RemoveDups.py
+++
@@ -1,1 +0,0 @@
-import LinkedList as ll
