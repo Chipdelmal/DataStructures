4c585c79f133da553e6aac486e786c49e3f628cd
chipdelmal <chipdelmal@gmail.com>
2019-01-07 17:45:47 -0800

    Adding array exercises definitions.

--- 02_LinkedList/01_RemoveDuplicates.py
+++ 02_LinkedList/01_RemoveDuplicates.py
@@ -4,12 +4,13 @@ from LinkedList import LinkedList
 class LinkedList_RemoveDuplicates(LinkedList):
     def __init__(self, values=[None]):
         super(LinkedList_RemoveDuplicates, self).__init__(values)
-        #super().__init__(self, values=[None])
 
-    # def removeDuplicatesHash():
-    #     return 1
-    # def removeDuplicatesNoBuffer():
-    #     return 1
+    def removeDuplicatesHash():
+        return 1
 
-llist2 = LinkedList_RemoveDuplicates([2,3,4])
+    def removeDuplicatesNoBuffer():
+        return 1
+
+
+llist2 = LinkedList_RemoveDuplicates([2, 3, 4, 3, 2, 1])
 llist2.printTraverseList()
