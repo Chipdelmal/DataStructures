ec638997cfe1693d00804c6d9e3f7d6925992924
chipdelmal <chipdelmal@gmail.com>
2019-01-08 17:57:17 -0800

    Working in the remove duplicates problem.

--- 02_LinkedList/00_main.py
+++ 02_LinkedList/00_main.py
@@ -4,16 +4,6 @@ node = ll.Node(10,None)
 node.data = 20
 node.next
 
-# class LinkedList(object):
-#     def __init__(self, values=[None]):
-#         tnode = ll.Node(10, None)
-#         print(tnode.data)
-#         self.__head = tnode
-#
-#     @property
-#     def head(self):
-#         return self.__head
-
 
 llist = ll.LinkedList([1,2,3,2,1])
 llist.head.data
--- 02_LinkedList/01_RemoveDuplicates.py
+++ 02_LinkedList/01_RemoveDuplicates.py
@@ -5,12 +5,33 @@ class LinkedList_RemoveDuplicates(LinkedList):
     def __init__(self, values=[None]):
         super(LinkedList_RemoveDuplicates, self).__init__(values)
 
-    def removeDuplicatesHash():
-        return 1
+    def removeDuplicatesHash(self):
+        if self.head is None:
+            # Linked list is empty
+            return None
+        else:
+            # Prime the list with the head, and the set with present values
+            currentNode = self.head
+            valuesPresent = {currentNode.data}
+            # Iterate the list changing the pointers if values are present
+            while currentNode.next is not None:
+                nextNode = currentNode.next
+                print("c: ", currentNode.data, "::", "n: ", nextNode.data)
+                if nextNode.data in valuesPresent:
+                    print("present: ", nextNode.data)
+                    # Would need to destruct next here if it was C++
+                    while nextNode.data in valuesPresent:
+                        currentNode.next = nextNode.next
+                        currentNode = currentNode.next
+                else:
+                    valuesPresent.add(nextNode.data)
+                    currentNode = currentNode.next
 
     def removeDuplicatesNoBuffer():
         return 1
 
 
 llist2 = LinkedList_RemoveDuplicates([2, 3, 4, 3, 2, 1])
+#llist2.printTraverseList()
+llist2.removeDuplicatesHash()
 llist2.printTraverseList()
