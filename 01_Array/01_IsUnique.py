15231ae56bcfc4f1fef33012b497c82f298a5d90
chipdelmal <chipdelmal@gmail.com>
2019-01-13 18:14:34 +0000

    "Permutation" now works with spaces and case insensitive. Just need to fix the import.

--- 01_Array/01_IsUnique.py
+++ 01_Array/01_IsUnique.py
@@ -4,17 +4,25 @@
 ##############################################################################
 
 
-def isUnique_Set(inputString, caseSensitive=True, spaces=True):
+def caseAndSpaceTreat(inputString, caseSensitive=True, spaces=True):
     '''
-    Returns true, if all the characters in the input string are unique.
-    This function uses an additional structure (set) to store the unique
-        characters already used.
+    Returns a lowercase string if case insensitive, and removes spaces
+        if necessary
     '''
     # String pre-treatment
     if caseSensitive == False:
         inputString = inputString.lower()
     if spaces == False:
         inputString = inputString.replace(" ", "")
+    return inputString
+
+def isUnique_Set(inputString, caseSensitive=True, spaces=True):
+    '''
+    Returns true, if all the characters in the input string are unique.
+    This function uses an additional structure (set) to store the unique
+        characters already used.
+    '''
+    inputString = caseAndSpaceTreat(inputString,caseSensitive,spaces)
     lettersSet = {inputString[0]}
     allUnique = True
     for i in range(1, len(inputString)):
@@ -33,10 +41,7 @@ def isUnique_NoStructs(inputString, caseSensitive=True, spaces=True):
     This function does not use any additional structures. It goes through
         the string element by element, checking for matches.
     '''
-    if caseSensitive == False:
-        inputString = inputString.lower()
-    if spaces == False:
-        inputString = inputString.replace(" ", "")
+    inputString = caseAndSpaceTreat(inputString,caseSensitive,spaces)
     allUnique = True
     for i in range(0, len(inputString)):
         for j in range(i + 1, len(inputString)):
@@ -52,11 +57,11 @@ def isUnique_NoStructs(inputString, caseSensitive=True, spaces=True):
 # Test and Debug
 ##############################################################################
 if __name__ == '__main__':
-    inputString = "abc dez xy"
-    case = False
-    spaces = True
-    uniqueA = isUnique_Set(inputString,caseSensitive=case, spaces=spaces)
-    uniqueB = isUnique_NoStructs(inputString,caseSensitive=case, spaces=spaces)
+    inputString = "aAbcdez xy"
+    case = True
+    spaces = False
+    uniqueA = isUnique_Set(inputString, caseSensitive=case, spaces=spaces)
+    uniqueB = isUnique_NoStructs(inputString, caseSensitive=case, spaces=spaces)
     print(
         "Sets: " + str(uniqueA) + "\n"
         + "NoStruct: " + str(uniqueB) + "\n"
--- 01_Array/02_Permutation.py
+++ 01_Array/02_Permutation.py
@@ -4,20 +4,50 @@
 ##############################################################################
 
 
-def permutation_Sort(inStringA, inStringB):
+def caseAndSpaceTreat(inputString, caseSensitive=True, spaces=True):
+    '''
+    Returns a lowercase string if case insensitive, and removes spaces
+        if necessary
+    '''
+    # String pre-treatment
+    if caseSensitive == False:
+        inputString = inputString.lower()
+    if spaces == False:
+        inputString = inputString.replace(" ", "")
+    return inputString
+
+def permutation_Sort(inStringA, inStringB, caseSensitive=True, spaces=True):
+    '''
+    Returns true if the two strings are permutations of one another.
+    This function works by sorting the two strings, and then comparing them
+        to check if the resulting structure is the same.
+    '''
+    inStringA = caseAndSpaceTreat(inStringA,caseSensitive,spaces)
+    inStringB = caseAndSpaceTreat(inStringB,caseSensitive,spaces)
     sortedA = sorted(inStringA)
     sortedB = sorted(inStringB)
     return (sortedA == sortedB)
 
 
 def countCharacters(inString):
+    '''
+    Returns a dictionary with the count of characters in the provided string.
+    '''
     tempDict = {}
     for char in set(inString):
         tempDict[char] = inString.count(char)
     return(tempDict)
 
 
-def permutation_Count(inStringA, inStringB):
+def permutation_Count(inStringA, inStringB, caseSensitive=True, spaces=True):
+    '''
+    Returns true if the two strings are permutations of one another.
+    This function generates an additional set per string to count the times
+        a character repeats itself in each of them (uses an auxiliary
+        "countCharacters" function).
+    '''
+    inStringA = caseAndSpaceTreat(inStringA,caseSensitive,spaces)
+    inStringB = caseAndSpaceTreat(inStringB,caseSensitive,spaces)
     countA = countCharacters(inStringA)
     countB = countCharacters(inStringB)
     return (countA == countB)
@@ -27,7 +57,9 @@ def permutation_Count(inStringA, inStringB):
 # Test and Debug
 ##############################################################################
 if __name__ == '__main__':
-    inStringA = "test"
+    case = True
+    spaces = True
+    inStringA = "test "
     inStringB = "etst"
-    permutation_Sort(inStringA, inStringB)
-    permutation_Count(inStringA, inStringB)
+    permutation_Sort(inStringA, inStringB, caseSensitive=case, spaces=spaces)
+    permutation_Count(inStringA, inStringB, caseSensitive=case, spaces=spaces)
