4a3050bc304c46f9961e3d72f59780e16d19f0bd
chipdelmal <chipdelmal@gmail.com>
2019-01-13 18:01:14 +0000

    "IsUnique" now works for case insensitive and spaces removal

--- 01_Array/01_IsUnique.py
+++ 01_Array/01_IsUnique.py
@@ -4,12 +4,17 @@
 ##############################################################################
 
 
-def isUnique_Set(inputString):
+def isUnique_Set(inputString, caseSensitive=True, spaces=True):
     '''
     Returns true, if all the characters in the input string are unique.
     This function uses an additional structure (set) to store the unique
         characters already used.
     '''
+    # String pre-treatment
+    if caseSensitive == False:
+        inputString = inputString.lower()
+    if spaces == False:
+        inputString = inputString.replace(" ", "")
     lettersSet = {inputString[0]}
     allUnique = True
     for i in range(1, len(inputString)):
@@ -22,12 +27,16 @@ def isUnique_Set(inputString):
     return(allUnique)
 
 
-def isUnique_NoStructs(inputString):
+def isUnique_NoStructs(inputString, caseSensitive=True, spaces=True):
     '''
     Returns true, if all the characters in the input string are unique.
     This function does not use any additional structures. It goes through
         the string element by element, checking for matches.
     '''
+    if caseSensitive == False:
+        inputString = inputString.lower()
+    if spaces == False:
+        inputString = inputString.replace(" ", "")
     allUnique = True
     for i in range(0, len(inputString)):
         for j in range(i + 1, len(inputString)):
@@ -43,9 +52,11 @@ def isUnique_NoStructs(inputString):
 # Test and Debug
 ##############################################################################
 if __name__ == '__main__':
-    inputString = "abcdezxy"
-    uniqueA = isUnique_Set(inputString)
-    uniqueB = isUnique_NoStructs(inputString)
+    inputString = "abc dez xy"
+    case = False
+    spaces = True
+    uniqueA = isUnique_Set(inputString,caseSensitive=case, spaces=spaces)
+    uniqueB = isUnique_NoStructs(inputString,caseSensitive=case, spaces=spaces)
     print(
         "Sets: " + str(uniqueA) + "\n"
         + "NoStruct: " + str(uniqueB) + "\n"
