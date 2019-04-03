32f2f974dc4dfd10b9d302e29f2ce530079b9c06
chipdelmal <chipdelmal@gmail.com>
2019-04-02 18:29:26 -0700

    Adding the solution to the URLify problem.

---
+++ 01_Array/03_URLify.py
@@ -0,0 +1,34 @@
+##############################################################################
+# 03: URLify
+#   Extensions:
+##############################################################################
+
+
+def urlify(inputString, spaceReplacement="%20"):
+    '''
+    Returns the string with the spaces replaced by the "spaceReplacement"
+        parameter.
+    '''
+    replaceLength = len(spaceReplacement)
+    inputLength = len(inputString)
+    spacesCount = inputString.count(" ")
+
+    tempArray = [None] * (inputLength + (replaceLength - 1) * spacesCount)
+
+    j = 0
+    for i, char in enumerate(inputString):
+        if char is " ":
+            for replace in spaceReplacement:
+                tempArray[j] = replace
+                j = j + 1
+        else:
+            tempArray[j] = char
+            j = j + 1
+    return tempArray
+
+
+##############################################################################
+# Test and Debug
+##############################################################################
+if __name__ == '__main__':
+    urlified = urlify("This is not a test", spaceReplacement="%20")
