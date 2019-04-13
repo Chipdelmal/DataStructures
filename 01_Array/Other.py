04d1f6c456450adc5054bdd2a4b8bf3df7328377
chipdelmal <chipdelmal@gmail.com>
2019-04-13 13:31:51 -0700

    A couple more problems

--- 01_Array/Other.py
+++ 01_Array/Other.py
@@ -27,7 +27,7 @@ def sockMerchant(n, ar):
     return total
 
 
-# countingValleys #############################################################
+# Counting Valleys ############################################################
 def countingValleys(n, s):
     steps = list(map(int, s.replace("U", " 1").replace("D", " -1").split()))
     zeroCross = 0
@@ -42,16 +42,31 @@ def countingValleys(n, s):
     return zeroCross
 
 
+# Repeated String #############################################################
+def repeatedString(s, n):
+    length = len(s)
+    countA = s.count("a")
+    partialCount = countA * (n // length)
+    total = partialCount + s[0:(n % length)].count("a")
+    return total
+
+
+
 ##############################################################################
 # Test and Debug
 ##############################################################################
 if __name__ == '__main__':
 
     input = [1, 2, 3, 4, 5]
-    print(rotLeft([1, 2, 3, 4, 5], 4))
+    rotLeft([1, 2, 3, 4, 5], 4)
 
     input = [10, 10, 10, 20, 20, 30, 50]
-    print(sockMerchant(len(input), input))
+    sockMerchant(len(input), input)
 
     s = "UDDDUUUUDDDU"
-    print(countingValleys(len(s),s))
+    countingValleys(len(s),s)
+
+
+    s = "aba"
+    n = 10
+    repeatedString(s,n)
